c3c444aed4e366b542e2c26ae2a04c933837d167
Ivan Nur Ilham Syah <ivannurilham60@gmail.com>
2024-05-03 20:30:59 +0700

    Initial Commit

---
+++ ExploreEventKit/ExploreEventKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploreEventKitApp.swift
+//  ExploreEventKit
+//
+//  Created by Ivan Nur Ilham Syah on 03/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ExploreEventKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
